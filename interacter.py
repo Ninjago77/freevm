a65fddb80891b930aca94bd9a08352514d3a19fb
Ninjago77 <shanvantharunmozhi@gmail.com>
2023-02-09 23:23:26 +0300

    changes

--- interacter.py
+++ interacter.py
@@ -1,11 +1,12 @@
 import socket,subprocess,requests,re,threading
 
-global docker_image
+global docker_image,prefix
+prefix = "koala"
 docker_image = "freevm:latest"
 steamninja77_at_gmail_dot_com_AUTHTOKEN = "2LPuB8ubpvx1OdIo1Rt9iqjhIUt_5gXEPS9LtmTMShYaJJYGx"
 steamninja77_at_gmail_dot_com_API_KEY = "2LSV3llU08Iliz33uygx1dnFvwg_5AiT9qDcbCu2Tci5CgsBr"
 
-def convert_str_to_snake_case(text:str) -> str:
+def convert_str_to_snake_case_and_add_prefix(text:str) -> str:
     text = text.lower().replace(" ","_")
     for symbol in list("!\"#$%&'()*+,-./:;<=>?@[\]^`{|}~"):
         text = text.replace(symbol,"_symbol_")
@@ -18,6 +19,8 @@ def convert_str_to_snake_case(text:str) -> str:
         text = text[1:]
     while "__" in text:
         text = text.replace("__","_")
+    if not text.startswith(f"{prefix}_"):
+        text = f"{prefix}_{text}"
     return text
     
 
@@ -40,7 +43,7 @@ def delete_container(name:str) -> None:
 
 def create_container(port:int,name:str) -> None:
     delete_container(name=name)
-    cmd = f"\"C:\Program Files\Docker\Docker\\resources\\bin\docker.exe\" run --env=PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin -p {port}:22 --label='org.opencontainers.image.ref.name=ubuntu' --label='org.opencontainers.image.version=22.04' --runtime=runc --name={name} -d {docker_image}"
+    cmd = f"\"C:\Program Files\Docker\Docker\\resources\\bin\docker.exe\" run -p {port}:22 --label='org.opencontainers.image.ref.name=ubuntu' --name={name} -d {docker_image}"
     subprocess.run(cmd,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
 
 def start_ngrok_tunnel(port:int,AUTHTOKEN:str) -> threading.Thread:
@@ -64,12 +67,12 @@ def get_ssh_command(API_KEY:str) -> str:
     match = re.match(r"^tcp://(?P<host>\d+\.tcp\.\w+\.ngrok\.io):(?P<port>\d+)$",public_url)
     host,port = match.group("host"),match.group("port")
 
-    ssh_cmd = f"ssh theuser@{host} -p {port}"
+    ssh_cmd = f"ssh koala@{host} -p {port}"
 
     return ssh_cmd
 
 def main(name:str,API_KEY:str,AUTHTOKEN:str) -> list[str,threading.Thread,str]:
-    name = convert_str_to_snake_case(text=name)
+    name = convert_str_to_snake_case_and_add_prefix(text=name)
     port = generate_free_port()
     thread = start_ngrok_tunnel(port=port,AUTHTOKEN=AUTHTOKEN)
     create_container(port=port,name=name)
@@ -78,12 +81,12 @@ def main(name:str,API_KEY:str,AUTHTOKEN:str) -> list[str,threading.Thread,str]:
 
 try:
     ssh_cmd,thread,name = main(
-        name="slash_test",
+        name="testit",
         API_KEY=steamninja77_at_gmail_dot_com_API_KEY,
         AUTHTOKEN=steamninja77_at_gmail_dot_com_AUTHTOKEN,
     )
     print(ssh_cmd)
 except KeyboardInterrupt:
-    thread.join()
+    # thread.join()
     delete_container(name)
 
